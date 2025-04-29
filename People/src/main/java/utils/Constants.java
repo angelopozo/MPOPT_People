02bf39f58167e45446beeeb2cf5a98973e7dfb60
angelopozo <apozo2027@gmail.com>
2025-04-29 09:32:51 +0200

    added constants class

---
+++ People/src/main/java/utils/Constants.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ *
+ * @author Angelo
+ */
+public class Constants {
+    public static final String ARRAYLIST = "ArrayList";
+    public static final String HASHMAP = "HashMap";
+    public static final String FILE = "File";
+    public static final String FILE_SERIALIZATION = "File (Serialization)";
+    public static final String SQL_DATABASE = "SQL - Database";
+    public static final String JPA_DATABASE = "JPA - Database";
+}
