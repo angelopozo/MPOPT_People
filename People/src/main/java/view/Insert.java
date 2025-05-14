d766132e175b79d6797f359f77c475b0b5f8aeb0
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-05-14 12:35:21 +0200

    had changed an error in insert

--- People/src/main/java/view/Insert.java
+++ People/src/main/java/view/Insert.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.Color;
 import static utils.DataValidation.calculateNifLetter;
 import static utils.DataValidation.isLetter;
 import static utils.DataValidation.isNumber;
@@ -12,20 +11,17 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashSet;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import model.dao.DAOFile;
-import model.entity.Person;
 import org.jdatepicker.DateModel;
 import org.jdatepicker.JDatePicker;
-import java.awt.dnd.DropTargetListener;
-
+import view.DropPhotoListener;
+import view.PromptSupport;
 
 /**
- * Interface used to register a person. It is mandatory to enter at least the
+ * Interface used to register a person. It is mandatory to enter at least the 
  * NIF and the name.
  * @author Francesc Perez
  * @version 1.1.0
@@ -34,11 +30,9 @@ public class Insert extends javax.swing.JDialog {
 
     public Insert(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
-
         initComponents();
-        PromptSupport promptSupport1 = new PromptSupport("Enter NIF number, letter is calculated", Nif);
+         PromptSupport promptSupport1 = new PromptSupport("Enter NIF number, letter is calculated", nif);
         PromptSupport promptSupport2 = new PromptSupport("Enter full name", name);
-
         DropPhotoListener d = new DropPhotoListener(photo, this);
         DropTarget dropTarget = new DropTarget(photo, d);
         insert.setEnabled(false);
@@ -55,16 +49,13 @@ public class Insert extends javax.swing.JDialog {
     public JTextField getNam() {
         return name;
     }
- 
-    
-    
 
     public JDatePicker getDateOfBirth() {
         return dateOfBirth;
     }
 
     public JTextField getNif() {
-        return Nif;
+        return nif;
     }
 
     public JLabel getPhoto() {
@@ -82,16 +73,15 @@ public class Insert extends javax.swing.JDialog {
         java.awt.GridBagConstraints gridBagConstraints;
 
         insert = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        name = new javax.swing.JTextField();
         reset = new javax.swing.JButton();
         photo = new javax.swing.JLabel();
-        NIF = new javax.swing.JLabel();
-        Nif = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        nif = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         dateOfBirth = new org.jdatepicker.JDatePicker();
-        jLabel11 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        name = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Insert - People v1.1.0");
@@ -103,18 +93,46 @@ public class Insert extends javax.swing.JDialog {
         insert.setMaximumSize(new java.awt.Dimension(187, 33));
         insert.setMinimumSize(new java.awt.Dimension(187, 33));
         insert.setPreferredSize(new java.awt.Dimension(187, 33));
-        insert.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                insertActionPerformed(evt);
-            }
-        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridy = 3;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 12);
         getContentPane().add(insert, gridBagConstraints);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setText("Name");
+        jLabel1.setMaximumSize(new java.awt.Dimension(100, 22));
+        jLabel1.setMinimumSize(new java.awt.Dimension(100, 22));
+        jLabel1.setPreferredSize(new java.awt.Dimension(100, 22));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 12);
+        getContentPane().add(jLabel1, gridBagConstraints);
+
+        name.setMaximumSize(new java.awt.Dimension(400, 22));
+        name.setMinimumSize(new java.awt.Dimension(400, 22));
+        name.setPreferredSize(new java.awt.Dimension(400, 22));
+        name.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                nameKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                nameKeyTyped(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 24);
+        getContentPane().add(name, gridBagConstraints);
 
         reset.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         reset.setText("RESET");
@@ -129,7 +147,7 @@ public class Insert extends javax.swing.JDialog {
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
-        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridy = 3;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
         gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 24);
@@ -157,31 +175,31 @@ public class Insert extends javax.swing.JDialog {
         gridBagConstraints.insets = new java.awt.Insets(24, 24, 12, 12);
         getContentPane().add(photo, gridBagConstraints);
 
-        NIF.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        NIF.setText("NIF");
-        NIF.setMaximumSize(new java.awt.Dimension(100, 22));
-        NIF.setMinimumSize(new java.awt.Dimension(100, 22));
-        NIF.setPreferredSize(new java.awt.Dimension(100, 22));
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel5.setText("NIF");
+        jLabel5.setMaximumSize(new java.awt.Dimension(100, 22));
+        jLabel5.setMinimumSize(new java.awt.Dimension(100, 22));
+        jLabel5.setPreferredSize(new java.awt.Dimension(100, 22));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.insets = new java.awt.Insets(24, 12, 12, 12);
-        getContentPane().add(NIF, gridBagConstraints);
+        getContentPane().add(jLabel5, gridBagConstraints);
 
-        Nif.setMaximumSize(new java.awt.Dimension(400, 22));
-        Nif.setMinimumSize(new java.awt.Dimension(400, 22));
-        Nif.setPreferredSize(new java.awt.Dimension(400, 22));
-        Nif.addKeyListener(new java.awt.event.KeyAdapter() {
+        nif.setMaximumSize(new java.awt.Dimension(400, 22));
+        nif.setMinimumSize(new java.awt.Dimension(400, 22));
+        nif.setPreferredSize(new java.awt.Dimension(400, 22));
+        nif.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
-                NifKeyPressed(evt);
+                nifKeyPressed(evt);
             }
             public void keyReleased(java.awt.event.KeyEvent evt) {
-                NifKeyReleased(evt);
+                nifKeyReleased(evt);
             }
             public void keyTyped(java.awt.event.KeyEvent evt) {
-                NifKeyTyped(evt);
+                nifKeyTyped(evt);
             }
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -191,7 +209,20 @@ public class Insert extends javax.swing.JDialog {
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.insets = new java.awt.Insets(24, 12, 12, 24);
-        getContentPane().add(Nif, gridBagConstraints);
+        getContentPane().add(nif, gridBagConstraints);
+
+        jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel8.setText("Date of Birth");
+        jLabel8.setMaximumSize(new java.awt.Dimension(150, 22));
+        jLabel8.setMinimumSize(new java.awt.Dimension(150, 22));
+        jLabel8.setPreferredSize(new java.awt.Dimension(150, 22));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 12);
+        getContentPane().add(jLabel8, gridBagConstraints);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 2, 8)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -199,7 +230,7 @@ public class Insert extends javax.swing.JDialog {
         jLabel2.setRequestFocusEnabled(false);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 6;
+        gridBagConstraints.gridy = 4;
         gridBagConstraints.gridwidth = 4;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.insets = new java.awt.Insets(12, 24, 12, 24);
@@ -210,82 +241,18 @@ public class Insert extends javax.swing.JDialog {
         dateOfBirth.setPreferredSize(new java.awt.Dimension(350, 22));
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridy = 2;
         gridBagConstraints.gridwidth = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 24);
         getContentPane().add(dateOfBirth, gridBagConstraints);
-
-        jLabel11.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel11.setText("Date of Birth");
-        jLabel11.setMaximumSize(new java.awt.Dimension(150, 22));
-        jLabel11.setMinimumSize(new java.awt.Dimension(150, 22));
-        jLabel11.setPreferredSize(new java.awt.Dimension(150, 22));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 12);
-        getContentPane().add(jLabel11, gridBagConstraints);
-
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel6.setText("NIF");
-        jLabel6.setMaximumSize(new java.awt.Dimension(100, 22));
-        jLabel6.setMinimumSize(new java.awt.Dimension(100, 22));
-        jLabel6.setPreferredSize(new java.awt.Dimension(100, 22));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(24, 12, 12, 12);
-        getContentPane().add(jLabel6, gridBagConstraints);
-
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel3.setText("Name");
-        jLabel3.setMaximumSize(new java.awt.Dimension(100, 22));
-        jLabel3.setMinimumSize(new java.awt.Dimension(100, 22));
-        jLabel3.setPreferredSize(new java.awt.Dimension(100, 22));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 12);
-        getContentPane().add(jLabel3, gridBagConstraints);
-
-        name.setMaximumSize(new java.awt.Dimension(400, 22));
-        name.setMinimumSize(new java.awt.Dimension(400, 22));
-        name.setPreferredSize(new java.awt.Dimension(400, 22));
-        name.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nameActionPerformed(evt);
-            }
-        });
-        name.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                nameKeyReleased(evt);
-            }
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                nameKeyTyped(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(12, 12, 12, 24);
-        getContentPane().add(name, gridBagConstraints);
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void showInsert() {
-        if (!name.getText().isEmpty() && !Nif.isEditable()) {
+        if (!name.getText().isEmpty() && !nif.isEditable()) {
             insert.setEnabled(true);
         } else {
             insert.setEnabled(false);
@@ -293,8 +260,8 @@ public class Insert extends javax.swing.JDialog {
     }
 
     private void resetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetActionPerformed
-        Nif.setEditable(true);
-        Nif.setText("");
+        nif.setEditable(true);
+        nif.setText("");
         name.setText("");
         photo.setIcon(null);
         //We reset the calendar date to the current date ...
@@ -311,43 +278,56 @@ public class Insert extends javax.swing.JDialog {
         insert.setEnabled(false);
     }//GEN-LAST:event_resetActionPerformed
 
-    private void NifKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_NifKeyTyped
+    private void nifKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nifKeyTyped
         if (!isNumber(evt.getKeyChar()) && evt.getKeyChar() != KeyEvent.VK_BACK_SPACE && evt.getKeyChar() != KeyEvent.VK_DELETE) {
             JOptionPane.showMessageDialog(this, "Type only numbers [0-9]", this.getTitle(), JOptionPane.ERROR_MESSAGE);
             evt.consume();
         }
-    }//GEN-LAST:event_NifKeyTyped
+    }//GEN-LAST:event_nifKeyTyped
+
+    private void nameKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nameKeyTyped
+        if (!isLetter(evt.getKeyChar()) && evt.getKeyChar() != KeyEvent.VK_BACK_SPACE && evt.getKeyChar() != KeyEvent.VK_DELETE) {
+            JOptionPane.showMessageDialog(this, "Type only uppercase or lowercase letters, hyphens, and whitespace.", this.getTitle(), JOptionPane.ERROR_MESSAGE);
+            evt.consume();
+        } else if (isLetter(evt.getKeyChar()) || evt.getKeyChar() == KeyEvent.VK_BACK_SPACE || evt.getKeyChar() == KeyEvent.VK_DELETE) {
+            showInsert();
+        }
+    }//GEN-LAST:event_nameKeyTyped
+
+    private void nameKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nameKeyReleased
+        showInsert();
+    }//GEN-LAST:event_nameKeyReleased
 
     private void photoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_photoMouseClicked
         photo.setIcon(null);
     }//GEN-LAST:event_photoMouseClicked
 
-    private void NifKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_NifKeyReleased
-        if (Nif.getText().length() == 8) {
-            Nif.setText(calculateNifLetter(Nif.getText()));
-            Nif.setEditable(false);
+    private void nifKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nifKeyReleased
+        if (nif.getText().length() == 8) {
+            nif.setText(calculateNifLetter(nif.getText()));
+            nif.setEditable(false);
             showInsert();
         }
-    }//GEN-LAST:event_NifKeyReleased
+    }//GEN-LAST:event_nifKeyReleased
 
-    private void NifKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_NifKeyPressed
-        if (Nif.getText().length() == 8) {
+    private void nifKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_nifKeyPressed
+        if (nif.getText().length() == 8) {
             evt.consume();
-            Nif.setText(calculateNifLetter(Nif.getText()));
-            Nif.setEditable(false);
+            nif.setText(calculateNifLetter(nif.getText()));
+            nif.setEditable(false);
             showInsert();
         }
-    }//GEN-LAST:event_NifKeyPressed
+    }//GEN-LAST:event_nifKeyPressed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel NIF;
-    private javax.swing.JTextField Nif;
     private org.jdatepicker.JDatePicker dateOfBirth;
     private javax.swing.JButton insert;
-    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JTextField name;
+    private javax.swing.JTextField nif;
     private javax.swing.JLabel photo;
     private javax.swing.JButton reset;
     // End of variables declaration//GEN-END:variables
